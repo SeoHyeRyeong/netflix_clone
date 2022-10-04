2791feff1615fe5b495c32b0fff48b1bd5790d29
SeoHyeRyeong <totomoomoo@naver.com>
2022-10-04 10:15:38 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/screen/home_screen.dart';
+import 'package:netflix_clone/widget/bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget{
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>{
+  late TabController controller;
+  Widget build(BuildContext context){
+    return MaterialApp(title: 'Netflix', theme: ThemeData(brightness: Brightness.dark, primaryColor: Colors.black, accentColor: Colors.white),
+    home: DefaultTabController(length: 4, child: Scaffold(body: TabBarView(
+        physics: NeverScrollableScrollPhysics(), children: <Widget>[
+          HomeScreen(),
+        Container(
+          child: Center(
+            child: Text('home'),
+          ),
+        ),
+        Container(
+          child: Center(
+            child: Text('search'),
+          ),
+        ),
+        Container(
+          child: Center(
+            child: Text('save'),
+          ),
+        ),
+        Container(
+          child: Center(
+            child: Text('more'),
+          ),
+        ),
+    ],
+    ),
+      bottomNavigationBar: Bottom(),
+    ),
+    )
+    );
+  }
+}
